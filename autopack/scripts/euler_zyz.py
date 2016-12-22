2ff84a9b2958575afb2a08217443057642410b49
Ludo <autin@scripps.edu>
2016-12-21 16:00:14 -0800

    no message

---
+++ autopack/scripts/euler_zyz.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 14 11:27:26 2016
+
+@author: ludo
+"""
+
+
+
+
+import transformation as tr
+eulers=[1, .7, .4,]
+
+m=tr.euler_matrix(1,0.7,0.4,axes='szyz')
+print m 
+#matrices=[???]
